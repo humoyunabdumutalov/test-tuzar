29721c6c2551e2d6e50b41b40d7b224f4d846306
humoyunabdumutalov <149642923+humoyunabdumutalov@users.noreply.github.com>
2026-03-01 18:14:39 +0500

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from threading import Thread
 
@@ -5,10 +6,12 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "Universal Test Boti 24/7 Onlayn! Server muvaffaqiyatli ishlayapti."
+    return "Universal Test Boti 24/7 Onlayn! Render serveri ishlayapti."
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    # Render o'zi bergan portni aniqlaymiz, topolmasa 8080 ni ishlatamiz
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
